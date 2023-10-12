6e0668564172f4cbbd36164db40298db96532dc0
LuisHerTo <20203028@aloe.ulima.edu.pe>
2023-10-12 12:43:55 -0500

    Commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,8 +10,7 @@ import"../styles/nuevoLibro.css";
 import { AppProps } from "next/app";
 import { DemoProvider } from "./context/demo";
 import { useEffect, useState } from "react";
-import "react-datepicker/dist/react-datepicker.css";
-import DatePicker from "react-datepicker";
+
 export default function MyApp({ Component, pageProps }) {
     return (
         <DemoProvider>
