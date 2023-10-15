6277004a4e1c208092f5806ae34144b0e9442dd0
ABGS2977 <20202977@aloe.ulima.edu.pe>
2023-10-15 13:42:07 -0500

    hola mundo2

--- src/pages/index.js
+++ src/pages/index.js
@@ -100,7 +100,7 @@ const Login = () => {
                     </button>
                     <div className="login_register">
                         <Link href="/registro">Registrar</Link>
-                        Hola mundo
+                        Hola mundo2
                     </div>
                 </form>
             </div>
